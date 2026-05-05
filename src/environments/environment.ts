12328e11f11eea6fb47f80d18c3ca514c3f0ff6a
sofia <sofiagracia000@gmail.com>
2026-05-05 18:59:58 +0200

    feat(#30): Added environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    // baseUrl: 'http://localhost:3000',
+
+    supabaseUrl: 'https://xjcdphpsoohbuftyozag.supabase.co',
+
+    supabaseKey: 'SUPABASE_KEY_REMOVED',
+
+};
